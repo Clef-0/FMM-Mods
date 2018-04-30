82db677c10249218ea5d9311a5bf9b89c6e37846
Clef <spaceyavin@gmail.com>
2018-04-30 16:22:43 +0100

    add H3Brute

---
+++ H3_Style_Brute_Weapons/H3Brute.bat
@@ -0,0 +1,25 @@
+ECHO Installing H3 Style Brute Weapons...
+
+
+color 0A
+
+if exist ..\..\..\maps\tags.dat (
+	GOTO Label0
+) else (
+	GOTO Label1
+)
+
+:Label0
+
+Type H3Brute.cmds | TagTool.exe ../../../maps/tags.dat
+
+ECHO Installation Complete ...
+
+CLEAR
+pause
+GOTO End
+
+:Label1
+ECHO WARNING Mod is not in the correct location.
+ECHO Install at: /mods/tagMods/H3_Style_Brute_Weapons
+ECHO Other install locations will be blocked by Windows.ECHO Contact /u/Vegetal for help
