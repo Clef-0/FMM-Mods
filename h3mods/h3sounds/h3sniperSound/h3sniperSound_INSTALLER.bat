5a5386ed09626aadf1d929908dbe143346285afc
Clef <spaceyavin@gmail.com>
2018-06-03 17:36:50 +0100

    add halo 3 sounds mods

---
+++ h3mods/h3sounds/h3sniperSound/h3sniperSound_INSTALLER.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+ECHO Installing ...
+
+color 0A
+
+if exist ..\..\..\..\..\maps\tags.dat (
+	GOTO Label0
+) else (
+	GOTO Label1
+)
+
+:Label0
+MOVE ..\..\..\..\..\maps\audio.dat audio.dat
+Type commands\h3sniperSound.cmds | TagTool.exe ../../../../../maps/tags.dat
+MOVE audio.dat ..\..\..\..\..\maps\audio.dat
+
+ECHO Installation Complete ...
+
+pause
+CLEAR
+GOTO End
+
+:Label1
+ECHO WARNING this mod is not in the correct location.
+ECHO It should be: Halo/mods/tagMods/h3mods/h3sounds/h3sniperSound
+ECHO Windows blocks mods to install if the game is in Program Files.
