1834228f7c609a0ce8ad473997a0b401ef2af1b1
clef <Clef-0@users.noreply.github.com>
2020-11-27 01:30:47 +0000

    Add H2A BR mod

---
+++ h2amods/H2aBR/H2aBRInstaller.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+ECHO Installing Halo 2 Anniversary Battle Rifle...
+color 0A
+
+if exist ..\..\..\..\maps\tags.dat (
+	GOTO Label0
+) else (
+	GOTO Label1
+)
+
+:Label0
+Type H2aBR.cmds | TagTool.exe ..\..\..\..\..\maps\tags.dat
+ECHO Installation Complete.
+pause
+GOTO End
+
+:Label1
+ECHO WARNING this mod needs to be in <HaloOnline>/mods/TagMods/h2amods/
+ECHO Windows may block mods if the game folder is in Program Files.
+pause
