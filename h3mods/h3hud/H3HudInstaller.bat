44860c2c7216afed0fdbf1e61e6c0b34f66cba3e
Satomi <Hordys8096@icloud.com>
2023-12-23 22:32:19 +0000

    Update H3 Mods
    - Fix FMM Previews
    - Clean-up Mod folders
    - Replace various upscaled bitmaps with Edited MCC versions on H3 Hud
    - Add Hit-Marker Support to H3 Hud

--- h3mods/h3hud/H3HudInstaller.bat
+++ h3mods/h3hud/H3HudInstaller.bat
@@ -29,7 +29,8 @@ if exist ..\..\..\..\maps\tags.dat (
 )
 
 :Label0
-Type H3Hud.cmds | TagTool.exe ..\..\..\..\maps\tags.dat
+Type H3Hud.cmds | Tagtool\TagTool.exe ..\..\..\..\maps\tags.dat
+Type Hud_Update.cmds | Tagtool\TagTool.exe ..\..\..\..\maps\tags.dat
 ECHO Installation Complete.
 pause
 GOTO End
--- h3mods/h3vehicles/H3VehiclesInstaller.bat
+++ h3mods/h3vehicles/H3VehiclesInstaller.bat
@@ -29,7 +29,7 @@ if exist ..\..\..\..\maps\tags.dat (
 )
 
 :Label0
-Type H3Vehicles.cmds | TagTool.exe ..\..\..\..\maps\tags.dat
+Type H3Vehicles.cmds | Tagtool\TagTool.exe ..\..\..\..\maps\tags.dat
 ECHO Installation Complete.
 pause
 GOTO End
