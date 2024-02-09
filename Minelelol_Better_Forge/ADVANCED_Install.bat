bb458911b0ffddf9e929eee3163d980d586031a4
Minelelol <89744342+Minelelol@users.noreply.github.com>
2024-02-09 11:17:41 +0700

    MOAR MAGNETS

--- Minelelol_Better_Forge/ADVANCED_Install.bat
+++ Minelelol_Better_Forge/ADVANCED_Install.bat
@@ -1,5 +1,7 @@
 copy /y ADVANCED\Items.xml ..\..\ui\web\screens\forge\object_creation
 copy /y BASIC\object_properties.js ..\..\ui\web\screens\forge\object_properties
 copy /y BASIC\materials.xml ..\..\ui\web\screens\forge\object_properties
+copy /y BASIC\For_troublemakers.md ..\..\forge
+copy /y BASIC\magnets.json ..\..\forge
 
 Type "commands\EFSKYBOX.cmds" | TagTool.exe ..\..\..\maps\tags.dat
--- Minelelol_Better_Forge/BASIC_Install.bat
+++ Minelelol_Better_Forge/BASIC_Install.bat
@@ -1,3 +1,5 @@
 copy /y BASIC\Items.xml ..\..\ui\web\screens\forge\object_creation
 copy /y BASIC\object_properties.js ..\..\ui\web\screens\forge\object_properties
 copy /y BASIC\materials.xml ..\..\ui\web\screens\forge\object_properties
+copy /y BASIC\For_troublemakers.md ..\..\forge
+copy /y BASIC\magnets.json ..\..\forge
